312acf7e62a78fbafc97d6c1921ebd8dad962dba
Pillifino <dinodizon073100@gmail.com>
2022-07-15 11:42:04 -0700

    added custome styling find out how to import custom styling page ito project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import About from './components/About';
 import Header from './components/Nav'
 
-import me from './images/me.jpg'
-
 
 // Pass users array to the List component as a prop
 export default function App() {
--- src/components/About.js
+++ src/components/About.js
@@ -10,18 +10,27 @@ function About(props) {
     <Container>
       <Row>
         <Col sm={8}>
-        <Card style={{ width: '100%' }}>
+        {/* <Card border='dark' style={{ width: '100%', height: '100%'}}>
       <Card.Body>
-        <Card.Title>About Me</Card.Title>
-        <Card.Text>
+        <Card.Title style={{fontSize:'30px'}}>About Me</Card.Title>
+        <Card.Text style={{fontSize:'20px'}}>
         Hello! My Name Is Dino Dizon and I am a junior software developer. I am currently enrolled in the UW Coding Camp program. I look forward to adding onto my skillset in hopes of becoming a fulltime software engineer!
         </Card.Text>
       </Card.Body>
-    </Card>
+    </Card> */}
+    <Card border="light" style={{ width: '100%', height:'100%' }}>
+        <Card.Header style={{backgroundColor:'black', color:'white', fontSize:'30px'}}>About Me</Card.Header>
+        <Card.Body style={{backgroundColor:'orange', color:'white'}}>
+          <Card.Text style={{fontSize:'22px' }}>
+          Hello! My Name Is Dino Dizon and I am a junior software developer. I am currently enrolled in the UW Coding Camp program. I look forward to adding onto my skillset in hopes of becoming a fulltime software engineer!
+          </Card.Text>
+        </Card.Body>
+      </Card>
         </Col>
-        <Col sm={4}><img src={me} alt = "this is an image of me"></img></Col> 
+        <Col sm={4}><img src={me} width="100%" alt = "this is an image of me"></img></Col> 
       </Row>
     </Container>
+    
   );
 }
 
