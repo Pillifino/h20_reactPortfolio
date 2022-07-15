2fd796145f7de89abb79ece7c6dc3361c604f6c3
Pillifino <dinodizon073100@gmail.com>
2022-07-15 07:10:02 -0700

    added navbar in app.js and boostrap link in index.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// Bootstrap imports
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-function App() {
+function ColorSchemesExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">My Portfolio</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">About Me</Nav.Link>
+            <Nav.Link href="#features">Portfolio</Nav.Link>
+            <Nav.Link href="#pricing">Contact</Nav.Link>
+            <Nav.Link href="#features">Portfolio</Nav.Link>
+            <Nav.Link href="#pricing">Resume</Nav.Link>
+
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
-export default App;
+export default ColorSchemesExample;
