6e4d5618edf5247d443fd15c33f11aea4affb419
Pillifino <dinodizon073100@gmail.com>
2022-07-20 18:30:48 -0700

    Fixed up the styling a bit more

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
-import NavTabs from './NavTabs'
-import About from './About';
-import Portfolio from './Portfolio'
-import Contact from './Contact'
-import Resume from './Resume'
-import Footer from './Footer'
+import React, { useState } from "react";
+import NavTabs from "./NavTabs";
+import About from "./About";
+import Portfolio from "./Portfolio";
+import Contact from "./Contact";
+import Resume from "./Resume";
+import Footer from "./Footer";
 
-// import '../Styles/Main.css'
-
+import '../Styles/Main.css'
+import '../Styles/Footer.css'
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('About');
+  const [currentPage, setCurrentPage] = useState("About");
 
   // TODO: Add a comment describing the functionality of this method
   const renderPage = () => {
-    if (currentPage === 'About') {
+    if (currentPage === "About") {
       return <About />;
     }
-    if (currentPage === 'Portfolio') {
+    if (currentPage === "Portfolio") {
       return <Portfolio />;
     }
-    if (currentPage === 'Contact') {
+    if (currentPage === "Contact") {
       return <Contact />;
     }
     return <Resume />;
@@ -29,16 +29,19 @@ export default function PortfolioContainer() {
   const handlePageChange = (page) => setCurrentPage(page);
   console.log(currentPage);
   return (
-    <body>
+    <>
       <header>
-      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+        <NavTabs
+          currentPage={currentPage}
+          handlePageChange={handlePageChange}
+        />
       </header>
       <main>
-      {renderPage()}
+        {renderPage()}
+      </main>
       <footer>
-      <Footer/>
-      </footer>
-    </main>
-    </body>
+          <Footer />
+        </footer>
+    </>
   );
 }
