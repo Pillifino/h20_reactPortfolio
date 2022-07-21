80ea171db898f15f16ccab3a7854b1f96572045b
Pillifino <dinodizon073100@gmail.com>
2022-07-20 18:35:20 -0700

    changed links to whitesmoke

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import "../Styles/Resume.css"
+
 export default function Contact() {
   return (
     <div>
       <h1>Resume Page</h1>
-      <a href='https://docs.google.com/document/d/16j67JlT4afIC_B4HlrmtFhCuA--uMJoKo1dlKiAuJss/edit?usp=sharing'>Click Here</a>
+      <a href='https://docs.google.com/document/d/16j67JlT4afIC_B4HlrmtFhCuA--uMJoKo1dlKiAuJss/edit?usp=sharing' target={"_blank"}>Click Here</a>
     </div>
   );
 }
